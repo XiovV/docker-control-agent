8ee1b5ad22946ba7f2026e2a64848de7b7e1d42e
XiovV <xiovv@tutanota.com>
2021-08-27 10:27:21 +0200

    removed -d

--- compile.sh
+++ compile.sh
@@ -7,4 +7,4 @@ git pull origin master
 echo "building new image"
 docker build -t xiovv/docker-control-agent:latest .
 echo "run running new container"
-docker run -d -p 5006:8080 --name=docker-control-agent -v /var/run/docker.sock:/var/run/docker.sock xiovv/docker-control-agent:latest
+docker run -p 5006:8080 --name=docker-control-agent -v /var/run/docker.sock:/var/run/docker.sock xiovv/docker-control-agent:latest

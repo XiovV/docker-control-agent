e5a846b4f918dd416666a6de66cd1592e3b1026b
XiovV <xiovv@tutanota.com>
2021-08-27 10:12:39 +0200

    script for building and running a new container

--- compile.sh
+++ compile.sh
@@ -1,2 +1,10 @@
-go build
-scp docker_control lab1@192.168.0.31:/home/lab1
+echo "stopping docker-control-agent"
+docker stop docker-control-agent
+echo "removing docker-control-agent"
+docker rm docker-control-agent
+echo "pulling latest changes"
+git pull origin master
+echo "building new image"
+docker build -t xiovv/docker-control-agent:latest .
+echo "run running new container"
+docker run -d -p 5006:8080 --name=docker-control-agent -v /var/run/docker.sock:/var/run/docker.sock xiovv/docker-control-agent:latest
